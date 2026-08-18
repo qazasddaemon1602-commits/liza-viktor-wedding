2533a102dff135c99826843c3b3a9cb4b3ae17bb
qazasddaemon1602-commits <qazasddaemon1602@gmail.com>
2026-08-18 08:27:58 +0200

    style: load train arrival presentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/globals.css';
+import './styles/train-arrival.css';
 
 const root = document.getElementById('root');
 
