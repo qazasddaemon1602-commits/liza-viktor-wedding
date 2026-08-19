8b96617a339e8dfc1c16dcc127241fff5ab1f290
qazasddaemon1602-commits <qazasddaemon1602@gmail.com>
2026-08-19 22:49:36 +0200

    feat: enable global interaction sound

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { installGlobalInteractionAudio } from '../lib/siteAudio';
 import { AppRoutes } from './routes';
 
 export function App() {
+  useEffect(() => installGlobalInteractionAudio(), []);
+
   return (
     <BrowserRouter>
       <AppRoutes />
